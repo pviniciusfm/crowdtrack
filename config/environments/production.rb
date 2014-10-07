8a724ce142b8808caeb562f2c365ab19fa626f36
Paulo Machado <pviniciusfm@gmail.com>
2014-10-07 01:22:32 -0300

    Fix unicorn/heroku issues with sass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -62,7 +62,10 @@ Rails.application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  #config.assets.precompile += %w( search.js )
+  config.assets.paths << Rails.root.join("pixel-admin-scss", "widgets")
+  config.assets.paths << Rails.root.join("pixel-admin-scss", "rtl")
+  config.assets.paths << Rails.root.join("pixel-admin-scss", "themes")
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
