86acbd16d8b1f5f73b60e16e007337ca2199edb6
Paulo Machado <pviniciusfm@gmail.com>
2014-10-08 02:31:38 -0300

    Initial build of SignUp Screen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def render_main_menu?(option)
+		option
+	end
+	# def title(page_title)
+ #  		content_for :title, page_title.to_s
+	# end
+	def body_append(body_class)
+		content_for :body_append, body_class.to_s
+	end
 end
